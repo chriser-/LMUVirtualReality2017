f9e56324b9c2438948b35334e5ddfaa218e75528
Christoph Winter <c.winter@tum.de>
2017-10-06 18:17:22 +0200

    gun pos debug

--- sources/Game.cpp
+++ sources/Game.cpp
@@ -232,7 +232,7 @@ void Game::Update()
 
 void Game::UpdateWand(Vec3f position, Quaternion orientation) const
 {
-	m_gun->GetTransform()->setTranslation(Vec3f(position.x(), position.y()+50.f, position.z()-1000));
+	m_gun->GetTransform()->setTranslation(Vec3f(position.x(), position.y(), position.z()+1000));
 	m_gun->GetTransform()->setRotation(orientation);
 }
 
